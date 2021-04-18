e86a35a2e4dd261cff5648ad69b9005b4738b329
Niyaz Nazipov <niaz.nazipoff@yandex.ru>
2021-04-18 14:56:57 +0300

    implemented login and register components

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,39 +2,38 @@
   <div
     class="pa-4"
   >
-    <v-form ref="loginForm" lazy-validation>
+    <v-form
+      ref="loginForm"
+      lazy-validation
+      class="d-flex flex-column align-center"
+    >
+      <v-col cols="12" lg="4" xl="4" class="py-1">
+        <v-text-field
+          v-model="username"
+          label="Логин"
+          :rules="isEmptyRule"
+        />
+      </v-col>
 
-        <v-row>
-          <v-col cols="12" lg="4" xl="4" class="py-1">
-            <v-text-field
-                v-model="username"
-                label="Имя пользователя"
-                :rules="isEmptyRule"
-            />
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col cols="12" lg="4" xl="4" class="py-0">
-            <v-text-field
-                v-model="password"
-                type="password"
-                label="Пароль"
-                :rules="isEmptyRule"
-            />
-          </v-col>
-        </v-row>
-        <v-col class="d-flex" cols="12" sm="6" xsm="12"></v-col>
-        <v-spacer></v-spacer>
-        <v-row>
-          <v-col class="d-flex" cols="12" lg="4" xl="4">
-            <v-btn
-                x-large block color="success"
-                @click="onSubmit">
-              Войти
-            </v-btn>
-          </v-col>
-        </v-row>
+      <v-col cols="12" lg="4" xl="4" class="py-0">
+        <v-text-field
+          v-model="password"
+          type="password"
+          label="Пароль"
+          :rules="isEmptyRule"
+        />
+      </v-col>
 
+      <v-col class="d-flex" cols="12" sm="6" xsm="12"></v-col>
+      <v-spacer></v-spacer>
+
+      <v-col class="d-flex" cols="12" lg="4" xl="4">
+        <v-btn
+          x-large block color="success"
+          @click="onSubmit">
+          Войти
+        </v-btn>
+      </v-col>
     </v-form>
   </div>
 </template>
@@ -71,11 +70,3 @@ export default {
   }
 }
 </script>
-
-<style scoped lang="sass">
-input
-  background-color: aquamarine
-
-button
-  background-color: bisque
-</style>
--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,25 +1,61 @@
 <template>
-  <div>
-    <h1>Sign Up</h1>
-    <form @submit.prevent="onSubmit">
-      <input
-          type="text"
-          v-model="username"
-      >
-      <input
-          type="text"
+  <div class="pa-4">
+    <v-form
+      v-model="valid"
+      ref="registerForm"
+      lazy-validation
+      class="d-flex flex-column align-center"
+    >
+      <v-col cols="12" lg="4" xl="4" class="py-1">
+        <v-text-field
           v-model="name"
-      >
-      <input
-          type="text"
-          v-model="password"
-      >
-      <button
+          label="Имя"
+          :rules="[registerRules.isEmpty]"
+        />
+      </v-col>
+      <v-col cols="12" lg="4" xl="4" class="py-0">
+        <v-text-field
+          v-model="username"
+          label="Логин"
+          :rules="[registerRules.isEmpty, registerRules.min]"
+        />
+      </v-col>
 
-      >
-        sign up
-      </button>
-    </form>
+      <v-col cols="12" lg="4" xl="4" class="py-0">
+        <v-text-field
+          v-model="password"
+          :type="showPassword ? 'text' : 'password'"
+          label="Пароль"
+          :rules="[registerRules.isEmpty, registerRules.min]"
+          :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+          @click:append="togglePasswordShow"
+        />
+      </v-col>
+
+      <v-col cols="12" lg="4" xl="4" class="py-0">
+        <v-text-field
+          v-model="verifyPassword"
+          :type="showPassword ? 'text' : 'password'"
+          label="Подтверждение пароля"
+          :rules="[registerRules.isEmpty, passwordMatch]"
+          :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+          @click:append="togglePasswordShow"
+        />
+      </v-col>
+
+      <v-col class="d-flex" cols="12" sm="6" xsm="12"></v-col>
+      <v-spacer></v-spacer>
+
+      <v-col class="d-flex" cols="12" lg="4" xl="4">
+        <v-btn
+          x-large block color="success"
+          :disabled="!valid"
+          @click="onSubmit"
+        >
+          Зарегистрироваться
+        </v-btn>
+      </v-col>
+    </v-form>
   </div>
 </template>
 
@@ -31,26 +67,38 @@ import {
 export default {
   name: "SignUp",
   data: () => ({
+    valid: false,
     username: "",
     password: "",
+    verifyPassword: "",
     name: "",
+    showPassword: false,
+    registerRules: {
+      isEmpty: value => !!value.trim() || "Заполните поле",
+      min: value => (value && value.length >= 4) || "Минимум 4 символа",
+    },
   }),
+  computed: {
+    passwordMatch() {
+      return () => this.password === this.verifyPassword || "Пароли не совпадают";
+    }
+  },
   methods: {
     ...mapActions([
         "signUp"
     ]),
     onSubmit() {
-      console.log("sign up click")
-      this.signUp({
-        username: this.username,
-        password: this.password,
-        name: this.name,
-      })
+      if (this.$refs.registerForm.validate()) {
+        this.signUp({
+          username: this.username,
+          password: this.password,
+          name: this.name,
+        })
+      }
+    },
+    togglePasswordShow() {
+      this.showPassword = !this.showPassword
     }
   }
 }
 </script>
-
-<style scoped>
-
-</style>
